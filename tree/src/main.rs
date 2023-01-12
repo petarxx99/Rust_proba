d130c270f4a7b25534c12e4947f9bdda89716250
“petarxx99” <petar.bokalovic@gmail.com>
2023-01-13 00:04:20 +0100

    Dodao sam testove za file_rank_to_broj, broj_to_rank_file.

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -11,8 +11,9 @@ pub fn broj_to_rank_file(mut broj: u8) -> (u8, u8){
     let prvih_6_bitova: u8 = (1<<6) - 1;
     broj &= prvih_6_bitova;
 
-    let rank = (broj>>3) + 1;
-    let file = broj % 8 + 1;
+    let broj_sa_8: u8 = broj >> 3;
+    let rank = broj_sa_8 + 1;
+    let file = broj - (broj_sa_8 << 3) + 1;
     (rank, file)
 }
 
@@ -24,7 +25,45 @@ fn main() {
     tabla.svi_legalni_potezi();
 }
 
+#[cfg(test)]
+mod main_test{
+    use crate::{broj_to_rank_file, file_rank_to_broj};
 
+    #[test]
+    fn test_63_rank_8_file_8(){
+        let (rank, file) = broj_to_rank_file(63);
+        assert_eq!(8, rank);
+        assert_eq!(8, file);
+    }
+
+    #[test]
+    fn test_41_rank_6_file_2(){
+        let (rank, file) = broj_to_rank_file(41);
+        assert_eq!(6, rank);
+        assert_eq!(2, file);
+    }
+
+    #[test]
+    fn test_3_rank_1_file_4(){
+        let (rank, file) = broj_to_rank_file(3);
+        assert_eq!(1, rank);
+        assert_eq!(4, file);
+    }
+
+    #[test]
+    fn test_10_rank_2_file_3(){
+        let (rank, file) = broj_to_rank_file(10);
+        assert_eq!(2, rank);
+        assert_eq!(3, file);
+    }
+
+
+    #[test]
+    fn rank_3_file_4_daje_19(){
+        let polje: u8 = file_rank_to_broj(4, 3);
+        assert_eq!(19, polje);
+    }
+}
 
 
 
